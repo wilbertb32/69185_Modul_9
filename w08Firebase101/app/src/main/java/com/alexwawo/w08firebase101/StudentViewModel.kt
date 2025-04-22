3f9137ff2530af65ba2bb54b57c5e4b33613386e
wilbertb32 <129371068+wilbertb32@users.noreply.github.com>
2025-04-22 14:04:31 +0700

    Bagian 1, Checkpoint 2

--- w08Firebase101/app/src/main/java/com/alexwawo/w08firebase101/StudentViewModel.kt
+++ w08Firebase101/app/src/main/java/com/alexwawo/w08firebase101/StudentViewModel.kt
@@ -37,6 +37,26 @@ class StudentViewModel : ViewModel() {
             }
     }
 
+    fun updateStudent(student: Student){
+        val data = mapOf(
+            "id" to student.id,
+            "name" to student.name,
+            "program" to student.program
+        )
+
+        db.collection("students").document(student.docId)
+            .set(data)
+            .addOnSuccessListener { fetchStudents() }
+            .addOnFailureListener { e -> Log.w("Firestore", "Error updating document", e) }
+    }
+
+    fun deleteStudent(student: Student) {
+        db.collection("students").document(student.docId)
+            .delete()
+            .addOnSuccessListener { fetchStudents() }
+            .addOnFailureListener { e -> Log.w("Firestore", "Error deleting document", e) }
+    }
+
     private fun fetchStudents() {
         db.collection("students")
             .get()
@@ -46,7 +66,8 @@ class StudentViewModel : ViewModel() {
                     val id = document.getString("id") ?: ""
                     val name = document.getString("name") ?: ""
                     val program = document.getString("program") ?: ""
-                    list.add(Student(id, name, program))
+                    val docId = document.id
+                    list.add(Student(id, name, program, docId))
                 }
                 students = list
             }
