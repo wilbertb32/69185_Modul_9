030b5062fc8ee65702e960318cccf450542032b6
wilbertb32 <129371068+wilbertb32@users.noreply.github.com>
2025-04-22 15:32:55 +0700

    Bagian 2, checkpoint 1

---
+++ w08Firebase101-multiplephonesinarray/app/src/main/java/com/alexwawo/w08firebase101/Student.kt
@@ -0,0 +1,10 @@
+package com.alexwawo.w08firebase101
+
+data class Student(
+    val id: String = "",
+    val name: String = "",
+    val program: String = "",
+    val phones: List<String> = emptyList(),
+    val docId : String = ""
+)
+
---
+++ w08Firebase101-multiplephonesinarray/app/src/main/java/com/alexwawo/w08firebase101/StudentViewModel.kt
@@ -0,0 +1,60 @@
+package com.alexwawo.w08firebase101
+
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class StudentViewModel : ViewModel() {
+    private val db = Firebase.firestore
+    var students by mutableStateOf(listOf<Student>())
+        private set
+
+    init {
+        fetchStudents()
+    }
+
+    fun addStudent(student: Student) {
+        val studentMap = hashMapOf(
+            "id" to student.id,
+            "name" to student.name,
+            "program" to student.program,
+            "phones" to student.phones
+        )
+
+        db.collection("students")
+            .add(studentMap)
+            .addOnSuccessListener {
+                Log.d("Firestore", "DocumentSnapshot added with ID: ${it.id}")
+                fetchStudents()
+            }
+            .addOnFailureListener { e ->
+                Log.w("Firestore", "Error adding document", e)
+            }
+    }
+
+    private fun fetchStudents() {
+        db.collection("students")
+            .get()
+            .addOnSuccessListener { result ->
+                val list = mutableListOf<Student>()
+                for (document in result) {
+                    val id = document.getString("id") ?: ""
+                    val name = document.getString("name") ?: ""
+                    val program = document.getString("program") ?: ""
+                    val phones = document.get("phones") as? List<String> ?: emptyList()
+                    list.add(Student(id, name, program, phones))
+                }
+                students = list
+            }
+            .addOnFailureListener { exception ->
+                Log.w("Firestore", "Error getting documents.", exception)
+            }
+    }
+}
+
